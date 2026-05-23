530f0eae9db9654f24a112b2a127e8344e2a58f9
Emile <62842896+emidblol@users.noreply.github.com>
2026-05-23 11:45:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,61 +3,101 @@
 // Retrieved 2026-05-23, License - CC BY-SA 4.0
 
 function findGetParameter(parameterName) {
-    var result = null,
-        tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
-    return result;
+  var result = null,
+    tmp = [];
+  location.search
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
+  return result;
 }
 
 
 const scoreBoard = {
-  _round: 0,
   _home: 0,
   _away: 0,
   range: [0, 99],
+  boldwinner() {
+    if (this._home > this._away) {
+      document.getElementById('1').classList.add('bold');
+    } else {
+      document.getElementById('1').classList.remove('bold');
+    }
+    if (this._away > this._home) {
+      document.getElementById('2').classList.add('bold');
+    } else {
+      document.getElementById('2').classList.remove('bold');
+    }
+  },
   set home(val) {
-    this._home = val;
-    document.querySelector('#team1').textContent = this._home;
+    let previous = this._home;
+
+    if (previous < val) {
+      document.querySelector('#team1').classList.add('good');
+      setTimeout(() => {
+        this._home = val;
+        document.querySelector('#team1').textContent = this._home;
+        document.querySelector('#team1').classList.remove('good');
+        this.boldwinner();
+      }, 500);
+    } else {
+      document.querySelector('#team1').classList.add('bad');
+      setTimeout(() => {
+        this._home = val;
+        document.querySelector('#team1').textContent = this._home;
+        document.querySelector('#team1').classList.remove('bad');
+        this.boldwinner();
+      }, 500);
+    }
   },
   set away(val) {
-    this._away = val;
-    document.querySelector('#team2').textContent = this._away;
-  },
-  set round(val) {
-    this._round = val;
-    document.querySelector('#roundnum').textContent = this._round;
+    let previous = this._away;
+    if (previous < val) {
+      document.querySelector('#team2').classList.add('good');
+      setTimeout(() => {
+        this._away = val;
+        document.querySelector('#team2').textContent = this._away;
+        document.querySelector('#team2').classList.remove('good');
+        this.boldwinner();
+      }, 500);
+    } else {
+      document.querySelector('#team2').classList.add('bad');
+      setTimeout(() => {
+        this._away = val;
+        document.querySelector('#team2').textContent = this._away;
+        document.querySelector('#team2').classList.remove('bad');
+        this.boldwinner();
+      }, 500);
+    }
   },
   checkRangeAndUpdate(value, operator, step) {
     // destructure max and min
     const [min, max] = this.range;
     // set getter to underscore value for accessing object
     const getter = `_${value}`;
-    if(operator === '+' && (this[getter] + step) - 1 < max) {
+    if (operator === '+' && (this[getter] + step) - 1 < max) {
       // if operator is add and the incrementation wont exceede max increment by step
       this[value] = this[getter] + step;
     }
-    if(operator === '-' && (this[getter] - step) + 1 > min) {
+    if (operator === '-' && (this[getter] - step) + 1 > min) {
       // if operator is sub and the decrementation wont go below min deincrement by step
-     this[value] = this[getter] - step;
+      this[value] = this[getter] - step;
     }
   },
-  homeplus:   ['home', '+', 1],
-  homeminus:  ['home', '-', 1],
-  awayplus:   ['away', '+', 1],
-  awayminus:  ['away', '-', 1],
-  roundplus:  ['round', '+', 1],
+  homeplus: ['home', '+', 1],
+  homeminus: ['home', '-', 1],
+  awayplus: ['away', '+', 1],
+  awayminus: ['away', '-', 1],
+  roundplus: ['round', '+', 1],
   roundminus: ['round', '-', 1]
 }
 
 function init() {
   const container = document.querySelector('.scorecontainer');
-  container.addEventListener('click', function(e) {
+  container.addEventListener('click', function (e) {
     // run function with params that match the buttons id
     scoreBoard.checkRangeAndUpdate.apply(scoreBoard, scoreBoard[e.target.id]);
   });
@@ -65,7 +105,7 @@ function init() {
   findGetParameter('team2') && (document.getElementById('2').textContent = findGetParameter('team2'));
 }
 document.addEventListener("DOMContentLoaded", () => {
-init();
+  init();
 })
 // OLD CODE
 
